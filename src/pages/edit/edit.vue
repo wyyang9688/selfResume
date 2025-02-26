3600ea2058ba8ad56765470831b734b5699f451c
浅色流光 <wyyang9688@qq.com>
2025-02-26 21:36:52 +0800

    feat(edit.vue): 添加图片选择功能并优化文件上传处理

--- src/pages/edit/edit.vue
+++ src/pages/edit/edit.vue
@@ -2,7 +2,7 @@
 <template>
     <div class="editPage">
         <div class="person" v-if="showKey == 'person'">
-            <div class="header center mt10">
+            <div class="header center mt10" @click="selectImg('')">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -57,6 +57,9 @@
                         </g>
                     </g>
                 </svg>
+            </div>
+            <div class="item" v-for="(item, index) in fileList" :key="index">
+                <image :src="item.url" class="resImg" mode="scaleToFill" />
             </div>
             <!-- <weditor /> -->
             <div class="fitem" v-for="(item, index) in pform" :key="index">
@@ -1061,6 +1064,189 @@
         }
         console.log(obj);
         com.setItem("jlObj", obj);
+        back()
+    };
+    interface Config {
+        BASE_URL: string;
+    }
+    const config: Config | undefined = inject("$config");
+    const fileList = ref<any>([
+        // {
+        //     url: "https://cdn.hentre.com/public/images/antfin/activity/bg_popup_tmym2.jpg",
+        //     name: ""
+        // }
+    ]);
+    const selectImg = (item: string = "") => {
+        console.log(item);
+        try {
+            // uni.showLoading();
+            com.onMsg(async (e: any) => {
+                if (e.src) {
+                    // const regex = /[^/]+$/;
+                    // const result =
+                    //     e.path.match(regex)[0].split(".")[0] + e.extension;
+                    // const resp = await http.upload<{
+                    //     code: Number;
+                    //     data: string;
+                    // }>(
+                    //     "/api/acc/integral/fileUpload?imageName=" + result,
+                    //     e.path,
+                    //     {}
+                    // );
+                    // com.cs(JSON.stringify(resp));
+                    // console.log(resp);
+                    // console.log(config!.BASE_URL + "/api/image/" + resp.data);
+                    fileList.value.push({
+                        url:
+                            config!.BASE_URL +
+                            "/api/image/" +
+                            JSON.parse(e.src).data,
+                        name: JSON.parse(e.src).data
+                    });
+                    com.cs(JSON.stringify(fileList.value));
+                    console.log(fileList.value);
+                }
+            });
+            com.sendMsg("cImg", {
+                token: uni.getStorageSync("local_token")
+            });
+            return;
+            my.chooseImage({
+                sourceType: ["album"],
+                count: 1,
+                sizeType: ["original"],
+                success: async (res: any) => {
+                    com.cs(res);
+                    console.log(res);
+                    console.log(JSON.stringify(res));
+                    if (res.tempFiles && (res.tempFiles as File[]).length > 0) {
+                        if ((res.tempFiles as File[])[0].size > 5242880) {
+                            // 300k=307200   1m=1048576    5m=5242880 tempFilePaths
+                            uni.showToast({
+                                title: "请将图片压缩至5m以内，再上传"
+                            });
+                            return;
+                        }
+                        try {
+                            com.onMsg((e: any) => {
+                                if (e.fileType) {
+                                    com.cs(e.fileType);
+                                }
+                            });
+                            com.sendMsg("fileType", {
+                                path: res.tempFilePaths[0]
+                            });
+                            my.detectFileType({
+                                filePath: res.tempFilePaths[0],
+                                success: async (re: any) => {
+                                    console.log(re);
+                                    com.cs(re);
+                                    const regex = /[^/]+$/;
+                                    const result =
+                                        res.tempFilePaths[0]
+                                            .match(regex)[0]
+                                            .split(".")[0] + re;
+                                    com.cs(result);
+                                    const resp = await http.upload<{
+                                        code: Number;
+                                        data: string;
+                                    }>(
+                                        "/api/acc/integral/fileUpload?imageName=" +
+                                            result,
+                                        res.apFilePaths[0],
+                                        res.tempFiles[0]
+                                    );
+                                    com.cs(JSON.stringify(resp));
+                                    console.log(resp);
+                                    console.log(
+                                        config!.BASE_URL +
+                                            "/api/image/" +
+                                            resp.data
+                                    );
+                                    fileList.value.push({
+                                        url:
+                                            config!.BASE_URL +
+                                            "/api/image/" +
+                                            resp.data,
+                                        name: resp.data
+                                    });
+                                    console.log(fileList.value);
+                                },
+                                fail(err: any) {
+                                    console.log(err);
+                                    com.cs("deteError" + JSON.stringify(err));
+                                }
+                            });
+                        } catch (error) {
+                            com.cs(
+                                "detectFileTypeError" + JSON.stringify(error)
+                            );
+                        }
+
+                        return;
+                        uni.compressImage({
+                            src: res.tempFilePaths[0],
+                            // compressLevel: 1,
+                            success: async (data) => {
+                                console.log(data);
+                                if (item == "") {
+                                    imgItem.value.src = data.tempFilePath;
+                                }
+
+                                uni.showLoading({
+                                    title: "图片上传中..."
+                                });
+                                try {
+                                    const res = await http.upload<{
+                                        code: Number;
+                                        data: string;
+                                    }>(
+                                        "/api/acc/integral/fileUpload",
+                                        data.tempFilePath
+                                    );
+                                    console.log(res);
+                                    if (res.code == 0) {
+                                        imgItem.value.receiveSrc =
+                                            getConfig().imgBasicUrl + res.data;
+                                        // formData.value.imageId = res.data;
+                                    } else {
+                                        console.warn(res);
+                                    }
+                                } catch (error) {
+                                    console.warn(error);
+                                }
+                            }
+                        });
+                    } else {
+                        com.cs("*********");
+                    }
+                    uni.hideLoading();
+                },
+                fail: (error: any) => {
+                    com.cs(JSON.stringify(error));
+                    uni.hideLoading();
+                    my.showToast({
+                        content: JSON.stringify(error) // 文字内容
+                    });
+
+                    console.log(JSON.stringify(error));
+                    // if (error.error == 2001 || error.error == 11) {
+                    //     uni.showToast({
+                    //         title: "当前用户已拒绝该项授权", // 文字内容
+                    //         icon: "none"
+                    //     });
+                    // } else if (error.error == 2002) {
+                    //     // user does not allow authorization
+                    //     // isPhoto_Per.value= true
+                    //     // my.alert({ content: "打开失败：当前用户已拒绝该项授权，如需打开请点击右上角菜单中的设置选项重新打开,或者前往支付宝app-点击【我的】 - 右上角设置 - 【隐私】 - 【系统权限管理】，在里面看看相机、相册权限"});
+                    // }
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            com.cs(JSON.stringify(error));
+            uni.hideLoading();
+        }
     };
     onLoad((op) => {
         //
@@ -1073,7 +1259,7 @@
         jlId.value = com.getItem("jlObj").id;
         id.value = op?.id || "";
         if (op?.key == "person") {
-            let pformAdd = com.getItem("pform");
+            let pformAdd = obj.personItem;
 
             console.log(pformAdd);
             if (pformAdd) {
